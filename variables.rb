817322edebd5caa707b17d53041a6726ed9a871c
askarf <ayalakarf@gmail.com>
2023-09-25 23:14:17 -0400

    variables

---
+++ variables.rb
@@ -0,0 +1,68 @@
+# Write a program that uses variables to store a first and last name, then prints the full name in one line using string concatenation (the + operator).
+# first_name = "Ayala"
+# last_name = "Karfunkel"
+# pp first_name + " " + last_name
+
+# Write a program that uses variables to store a first and last name, then prints the full name in one line using string interpolation (the #{} operator).
+
+# first_name = "Ayala"
+# last_name = "Karfunkel"
+# pp "#{first_name} #{last_name}"
+
+# Write a program that asks the user to input a word. If the word is "marco", print "polo".
+
+# pp "please enter a word:"
+# input = gets.chomp
+# if input == "marco"
+#     puts "polo"
+# end
+
+# Write a program that uses variables to store three different colors, then prints out a sentence using the colors with string concatenation (the + operator).
+# color1 = "Red"
+# color2 = "Blue"
+# color3 = "Black"
+# pp "my three favorite colors are " + color1 + ", " + color2 + "and " + color3 + "."
+
+
+# Write a program that uses variables to store three different colors, then prints out a sentence using the colors with string interpolation (the #{} operator).
+
+# color1 = "Red"
+# color2 = "Blue"
+# color3 = "Black"
+# pp "My three favorite colors are #{color1}, #{color2}, and #{color3}"
+
+# Write a program that asks the user to enter a name. If the name is not "Santa", print "You're not Santa."
+
+# pp "please enter your name:"
+# input = gets.chomp
+# if input != "Santa"
+#     puts "your not santa!"
+# end
+
+# Write a program that uses variables to store a book's title and author, then prints out a sentence using that information with string concatenation (the + operator).
+
+# book_title = "I Want to go Home"
+# book_author = "Gordon Kormon"
+# pp "my favourite book is " + book_title + " by " + book_author + "."
+
+# Write a program that uses variables to store a book's title and author, then prints out a sentence using that information with string interpolation (the #{} operator).
+# book_title = "I Want to go Home"
+# book_author = "Gordon Kormon"
+# pp "my favourite book is #{book_title} by #{book_author}"
+
+# Write a program that asks the user to enter a password. If the password is "Joshua", the program responds "Shall we play a game?". For any other password, the program responds "Access denied"
+
+# pp "Please enter a password:"
+# password = gets.chomp
+# if password = "Joshua"
+#     puts "Shall we play a game?"
+# else
+#     puts "Access denied"
+# end
+
+# Write a program that uses variables to store the names of three cities, then prints out a sentence using that information with string concatenation (the + operator).
+
+city1 = "Toronto"
+city2 = "Chicago"
+city3 = "Jerusalem"
+pp "my three favorite cities are " + city1 + ", " + city2 + " and " + city3
